39dbe1e4589e8c49a408273202c804a5cae2dfa7
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-01-09 11:01:14 -0300

    resolvendo cors

--- apiVendas/Startup.cs
+++ apiVendas/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -24,6 +23,13 @@ namespace apiVendas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //cors
+            services.AddCors(options => options.AddDefaultPolicy(builder => {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
             services.AddDbContext<DataContext>(options =>
                        options.UseSqlite(
                            Configuration.GetConnectionString("ApiConnection")
@@ -55,16 +61,20 @@ namespace apiVendas
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apiVendas v1"));
-            }
+            }*/
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apiVendas v1"));
 
             app.UseAuthentication();
             
             app.UseRouting();
+            
+            app.UseCors();//cors
 
             app.UseAuthorization();
 
